fd8f9118bc6eb361a4c59927f43829ddd631bf06
Matheus Guilherme <matheus.gsousa02@gmail.com>
2024-05-30 15:24:32 -0300

    Fix

--- lib/services/service_category_services.dart
+++ lib/services/service_category_services.dart
@@ -3,10 +3,18 @@ import 'package:http/http.dart' as http;
 import 'package:service_app/models/service_category.dart';
 
 class ServiceCategoryServices {
-  final String _baseUrl = 'http://10.0.2.2:5120/ServiceCategory/';
+  final String _baseUrl = 'http://10.0.2.2:5120/ServiceCategory';
 
   Future<List<ServiceCategory>> getByUserProfileId(int userProfileId) async {
-    return _getRequest('GetByUserProfileId', userProfileId);
+    return _getRequest('/GetByUserProfileId', userProfileId);
+  }
+
+  Future<ServiceCategory> addServiceCategory(ServiceCategory request) async {
+    return _postRequest('', request);
+  }
+
+  Future<bool> deleteServiceCategory(int serviceCategoryId) async {
+    return _deleteRequest('/SetDeleted', serviceCategoryId);
   }
 
   Future<List<ServiceCategory>> _getRequest(String path, int id) async {
@@ -19,6 +27,37 @@ class ServiceCategoryServices {
           .map((serviceCategoryJson) =>
               ServiceCategory.fromJson(serviceCategoryJson))
           .toList();
+    } else {
+      _handleError(response.body);
+    }
+    throw Exception('Não foi possível completar a requisição para $path.');
+  }
+
+  Future<ServiceCategory> _postRequest(
+      String path, ServiceCategory request) async {
+    var url = Uri.parse('$_baseUrl$path');
+    var response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(request.toJson()),
+    );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return ServiceCategory.fromJson(jsonDecode(response.body));
+    } else {
+      _handleError(response.body);
+    }
+    throw Exception('Não foi possível completar a requisição para $path.');
+  }
+
+  Future<bool> _deleteRequest(String path, int id) async {
+    var url = Uri.parse('$_baseUrl/$id$path');
+    var response = await http.delete(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(true),
+    );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return true;
     } else {
       _handleError(response.body);
     }
